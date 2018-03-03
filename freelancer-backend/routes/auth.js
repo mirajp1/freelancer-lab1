b1275d88afee14c7ba726032768ef9f4a6dc3466
mirajp1 <mirajp1@gmail.com>
2018-03-03 14:34:09 -0800

    basic freelancer structure for frontend and backend

---
+++ freelancer-backend/routes/auth.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const validator = require('validator');
+
+const router = new express.Router();
+
+function validateSignUp(data){
+
+    let isValid=true;
+    let errors={};
+
+    if(!data || !validator.isEmail(data.email)){
+        isValid=false;
+        errors.email='Email Required in correct format';
+    }
+
+
+    if(!data || !data.password.trim().length<6){
+        isValid=false;
+        errors.email='Password needs to be atleast 6 characters long';
+    }
+
+    if(!data ||  !data.name.trim().length===0){
+        isValid=false;
+        errors.email='Name required';
+    }
+
+    return {
+        success: isValid,
+        errors
+    };
+
+
+}
+
+
+function validateLoginForm(data) {
+    let isValid=true;
+    let errors={};
+
+    if (!data || data.email.trim().length === 0) {
+        isValid = false;
+        errors.email = 'Email Required';
+    }
+
+    if (!data || data.password.trim().length === 0) {
+        isValid = false;
+        errors.password = 'Please provide your password.';
+    }
+
+
+
+    return {
+        success: isValid,
+        errors
+    };
+}
+
+router.post('/signup', (req, res) => {
+    let validation = validateSignupForm(req.body);
+    if (!validation.success) {
+        return res.status(400).json({
+            success: false,
+            errors: validationResult.errors
+        });
+    }
+
+    return res.status(200).end();
+});
+
+router.post('/login', (req, res) => {
+    const validation = validateLoginForm(req.body);
+    if (!validation.success) {
+
+        return res.status(400).json({
+            success: false,
+            errors: validation.errors
+        });
+    }
+
+    return res.status(200).end();
+});
+
+
+module.exports = router;
